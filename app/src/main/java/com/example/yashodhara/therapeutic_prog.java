a90683e7d2a9e9a88a31ae62e9e48bba1e3d6fb2
shehanai <shehanai@zone24x7.com>
2021-10-06 23:17:01 +0530

    therapeutic_prog and other_prog UIs

---
+++ app/src/main/java/com/example/yashodhara/therapeutic_prog.java
@@ -0,0 +1,14 @@
+package com.example.yashodhara;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class therapeutic_prog extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_therapeutic_prog);
+    }
+}
